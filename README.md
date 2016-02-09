1ee70a8144b373359a6d4badf7c20ac1c7c5d078
Jean-Sebastien C Ney <jeansebastien.ney@gmail.com>
2016-02-09 13:51:46 +0100

    Handle default
    configuration can now have `defaultVersion` option to specify a default
    version.

--- README.md
+++ README.md
@@ -7,11 +7,12 @@
 We want to keep our API clean being able to call /users or /users/2/follow/1 without adding version number
 /api/v1/users and api/v2/users
 
-with this Hapi.plugin you can just pass the pattern and header 
+with this Hapi.plugin you can just pass the pattern and header
 ```javascript
 {
     register: require('hapi-versioning'),
     options: {
+        defaultVersion: 'v0',
         pattern: /^(v[1-9])$/,
         header: 'ourversion'
     }
@@ -21,7 +22,7 @@ with this Hapi.plugin you can just pass the pattern and header
 That will check the header (only if its not set in the url) on evevry request and will redirect to thr right path.
 
 for ex'
-```javascript 
+```javascript
 {
     register: require('hapi-versioning'),
     options: {} //set to default header.version = v1
--- src/plugin.js
+++ src/plugin.js
@@ -4,21 +4,30 @@ exports.register = function(plugin, options, next){
   //Check if header contains version
   var DEFAULT_HEADER_KEY = 'version';
   var DEFAULT_PATTERN = /^(v[1-9])$/;
-  var versionHeader = options.header || DEFAULT_HEADER_KEY;
+  var versionHeader = (options.header || DEFAULT_HEADER_KEY).toLowerCase();
   var pattern = options.pattern || DEFAULT_PATTERN;
 
   plugin.ext('onRequest', function(request, reply) {
     var urlPath = request.url.pathname.split('/');
-    urlPath[0] === '' ? urlPath.shift() : null;
-    var reqHeader = request.headers[versionHeader];
-    var version = reqHeader.match(pattern);
+    if (urlPath[0] === '') urlPath.shift();
+    const version = getVersion(request.headers[versionHeader], pattern);
 
     if (version && !pattern.test(urlPath[0])){
-      urlPath.unshift('', version[1]);
+      urlPath.unshift('', version);
       request.setUrl(urlPath.join('/') + (request.url.search || ''));
     }
 
     reply.continue();
   });
   next();
+
+  /**
+   * @function getVersion
+   * returns matching version in the header or default version, if defined
+   */
+  function getVersion(header, pattern) {
+    const match = header.match(pattern);
+    if (match) return match[1];
+    return options.defaultVersion;
+  }
 };
