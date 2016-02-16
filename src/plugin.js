c7e32f56a73df6d1ed2e2a244af11197eef218bd
Jean-Sebastien C Ney <jeansebastien.ney@gmail.com>
2016-02-16 15:17:20 +0100

    add aliases

--- src/plugin.js
+++ src/plugin.js
@@ -4,13 +4,14 @@ exports.register = function(plugin, options, next){
   //Check if header contains version
   var DEFAULT_HEADER_KEY = 'version';
   var DEFAULT_PATTERN = /^(v[1-9])$/;
+  var aliases = options.aliases || [];
   var versionHeader = (options.header || DEFAULT_HEADER_KEY).toLowerCase();
   var pattern = options.pattern || DEFAULT_PATTERN;
 
   plugin.ext('onRequest', function(request, reply) {
     var urlPath = request.url.pathname.split('/');
     if (urlPath[0] === '') urlPath.shift();
-    const version = getVersion(request.headers[versionHeader], pattern);
+    var version = getVersion(request.headers[versionHeader], pattern);
 
     if (version && !pattern.test(urlPath[0])){
       urlPath.unshift('', version);
@@ -22,12 +23,28 @@ exports.register = function(plugin, options, next){
   next();
 
   /**
+   * @function getAlias
+   * returns alias reference, if one, or returns self
+   */
+  function getAlias (key) {
+    // return key if key is a reference
+    if (aliases[key]) return key;
+    var res = key;
+    Object.keys(aliases).some(function (e) {
+      if (aliases[e].indexOf(key) > -1) {
+        res = e;
+        return true;
+      }
+    })
+    return res;
+  }
+
+  /**
    * @function getVersion
    * returns matching version in the header or default version, if defined
    */
   function getVersion(header, pattern) {
-    const match = header.match(pattern);
-    if (match) return match[1];
-    return options.defaultVersion;
+    var match = header.match(pattern);
+    return getAlias(match ? match[1] : options.defaultVersion);
   }
 };
